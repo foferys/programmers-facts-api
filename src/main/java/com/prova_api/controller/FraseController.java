8c9b18e2c0bbbd048872c20eac2f2bbd73d9e25c
foferys <foferys@gmail.com>
2025-09-08 17:43:19 +0200

    ref. phrases endpoints

--- src/main/java/com/prova_api/controller/FraseController.java
+++ src/main/java/com/prova_api/controller/FraseController.java
@@ -1,25 +1,25 @@
 package com.prova_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import com.prova_api.phrases.Phrase;
 import com.prova_api.services.PhraseRepository;
 
-import java.util.Collections;
+import java.net.http.HttpHeaders;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Cache.Cachecontrol;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
-@RequestMapping("/getPhrase")
+@RequestMapping("/v1/phrases")
 public class FraseController {
 
 
@@ -28,8 +28,8 @@ public class FraseController {
     private PhraseRepository phrasesRepository;
 
     
-    
-    @GetMapping("/all")
+
+    @GetMapping("/")
     public Map<String, List<Phrase>> getAll() {
 
         List<Phrase> phrases = phrasesRepository.findAll();
@@ -61,14 +61,16 @@ public class FraseController {
  
 
     @GetMapping("/random")
-    public Map<String, Phrase> getRandom() {
+    public ResponseEntity<Map<String, Phrase>> getRandom() {
 
         Phrase phrase = phrasesRepository.findRandomPhrase();
 
         Map<String, Phrase> response = new HashMap<>();
         response.put("data", phrase);
 
-        return response;
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.noStore())
+                .body(response);
     }
 
 
